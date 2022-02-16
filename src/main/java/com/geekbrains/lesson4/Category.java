4d35c20ece7cbc1433135b992f1a19929b19a909
Asus <megabart57@gmail.com>
2022-02-17 01:59:27 +0300

    lesson4

---
+++ src/main/java/com/geekbrains/lesson4/Category.java
@@ -0,0 +1,55 @@
+package com.geekbrains.lesson4;
+
+import javax.persistence.*;
+import java.util.List;
+
+
+@Entity
+@Table  (name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(mappedBy = "category_id")
+    private List<Product> products;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", products=" + products +
+                '}';
+    }
+}
+
---
+++ src/main/java/com/geekbrains/lesson4/Main.java
@@ -0,0 +1,18 @@
+package com.geekbrains.lesson4;
+
+public class Main {
+
+
+
+
+    public static void main(String[] args) {
+
+
+        AddProduct.newProduct("fruit", "ananas", 120);
+        FindProduct.find(4);
+        ListProduct.list("fruit");
+        DeleteProduct.delete(5);
+
+
+    }
+}
