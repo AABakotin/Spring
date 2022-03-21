5fbaddd3eae39fb5fea823b894300864e6621a75
Bakotin Anton <85903476+AABakotin@users.noreply.github.com>
2022-03-21 13:43:49 +0300

    lesson9

--- src/main/resources/db/migration/V1__initialize.sql
+++ src/main/resources/db/migration/V1__initialize.sql
@@ -1,60 +1,89 @@
+SET FOREIGN_KEY_CHECKS = 0;
 
-USE geek_db;
+DROP TABLE IF EXISTS roles;
 
-DROP TABLE IF EXISTS categories;
-
-CREATE TABLE categories (
+CREATE TABLE roles (
   id int(11) NOT NULL AUTO_INCREMENT,
   name varchar(50) DEFAULT NULL,
   PRIMARY KEY (id)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
-INSERT INTO categories (name)
-VALUES('Vegatable'),('Friut'),('Meat');
+INSERT INTO roles (name)
+VALUES
+('ROLE_EMPLOYEE'),('ROLE_MANAGER'),('ROLE_ADMIN');
 
-DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS users;
 
-CREATE TABLE products (
+CREATE TABLE users (
   id int(11) NOT NULL AUTO_INCREMENT,
-  name varchar(50) NOT NULL,
+  username varchar(50) NOT NULL,
+  password char(80) NOT NULL,
+  first_name varchar(50) NOT NULL,
+  last_name varchar(50) NOT NULL,
+  email varchar(50) NOT NULL,
   PRIMARY KEY (id)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
-INSERT INTO products (name)
+INSERT INTO users (username,password,first_name,last_name,email)
 VALUES
-('tomato'),
-('orange'),
-('corn'),
-('beef'),
-('apple'),
-('potato'),
-('chicken');
+('alex','$2a$10$CGamJWPYokRss2FX6WcBJOgFIDKyUoCYnrGtepfVQyDSTdngSxrzK','Alex','GeekBrains','alex@gb.com');
 
-DROP TABLE IF EXISTS products_categories;
+DROP TABLE IF EXISTS users_roles;
 
-CREATE TABLE products_categories (
-  product_id int(11) NOT NULL,
-  category_id int(11) NOT NULL,
+CREATE TABLE users_roles (
+  user_id int(11) NOT NULL,
+  role_id int(11) NOT NULL,
 
-  PRIMARY KEY (product_id, category_id),
+  PRIMARY KEY (user_id, role_id),
 
-  KEY FK_ROLE_idx (category_id),
+  KEY FK_ROLE_idx (role_id),
 
-  CONSTRAINT FK_PRODUCT_05 FOREIGN KEY (product_id)
-  REFERENCES products (id)
+  CONSTRAINT FK_USER_05 FOREIGN KEY (user_id)
+  REFERENCES users (id)
   ON DELETE NO ACTION ON UPDATE NO ACTION,
 
-  CONSTRAINT FK_CATEGORY FOREIGN KEY (category_id)
-  REFERENCES categories (id)
+  CONSTRAINT FK_ROLE FOREIGN KEY (role_id)
+  REFERENCES roles (id)
   ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO products_categories (product_id, category_id)
+INSERT INTO users_roles (user_id, role_id)
 VALUES
 (1, 1),
-(2, 2),
-(3, 1),
-(4, 3),
-(5, 2),
-(6, 1),
-(7, 3);
+(1, 2),
+(1, 3);
+
+DROP TABLE IF EXISTS students;
+
+CREATE TABLE students (
+	id int(11) NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100) DEFAULT NULL,
+    PRIMARY KEY(id)
+) ENGINE=InnoDB CHARSET=utf8;
+
+DROP TABLE IF EXISTS courses;
+
+CREATE TABLE courses (
+	id int(11) NOT NULL AUTO_INCREMENT,
+    title VARCHAR(100) DEFAULT NULL,
+    PRIMARY KEY(id)
+) ENGINE=InnoDB CHARSET=utf8;
+
+DROP TABLE IF EXISTS students_courses;
+
+CREATE TABLE students_courses (
+	student_id int(11) NOT NULL,
+    course_id int(11) NOT NULL,
+
+    PRIMARY KEY (student_id, course_id),
+
+    CONSTRAINT FK_STUDENT FOREIGN KEY (student_id)
+    REFERENCES students (id)
+    ON DELETE NO ACTION ON UPDATE NO ACTION,
+
+    CONSTRAINT FK_COURSE FOREIGN KEY (course_id)
+    REFERENCES courses (id)
+    ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB CHARSET=utf8;
+
+SET FOREIGN_KEY_CHECKS = 1;
